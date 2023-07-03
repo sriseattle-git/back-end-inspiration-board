476e29abac528652fcfddc054cd1ae151252a04c
Sriram R <sriram@msn.com>
2023-07-02 22:13:00 -0700

    back-end initial setup done

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,7 @@
 from app import db
+from sqlalchemy import Column, ForeignKey, Integer, Table
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
