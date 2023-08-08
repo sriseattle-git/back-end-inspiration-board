70b0fea210027b539f0490490e24d1fd35e42046
Sriram R <sriram@msn.com>
2023-08-07 19:41:25 -0700

    Added app launch cmd line parameter for testing to indicate when to use local postgres db

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,17 @@ db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("NEON_DATABASE_URI")
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("NEON_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
