7a3c5d54ea17099b0aa782b4bd3d60a3060e631a
Sriram R <sriram@msn.com>
2023-08-07 18:51:38 -0700

    Updated app.py with Neon DB connection

--- app/app.py
+++ app/app.py
@@ -13,8 +13,8 @@ def create_app():
     app = Flask(__name__)
     
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("NEON_DATABASE_URI")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("NEON_DATABASE_URI")
     
     db.init_app(app)
     migrate.init_app(app, db)
